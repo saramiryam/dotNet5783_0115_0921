9609ec645b23a5dd695e6987402f2b1bfc9af230
saramiryam <2141saram@gmail.com>
2022-10-30 23:55:14 +0200

    first chance and pull

---
+++ Stage0/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace exe1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NewMethod();
+
+            Console.ReadKey();
+        }
+
+        private static void NewMethod()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine($"{name} , welcome to my first console application");
+        }
+    }
+}
+
