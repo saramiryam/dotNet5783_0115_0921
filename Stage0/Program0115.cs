f48609e4c80b72fbc8348676a0afb9433db65f1d
saramiryam <2141saram@gmail.com>
2022-11-03 00:16:22 +0200

    update no.1

--- Stage0/Program0115.cs
+++ Stage0/Program0115.cs
@@ -1,21 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-namespace exe1
+namespace stage0
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
-            NewMethod();
-
+            welcome0115();
+            welcome0921();
             Console.ReadKey();
-        }
 
-        private static void NewMethod()
+        }
+        static partial void welcome0921();
+
+        private static void welcome0115()
         {
             Console.Write("Enter your name: ");
             string name = Console.ReadLine();
-            Console.WriteLine($"{name} , welcome to my first console application");
+            Console.WriteLine(name + ", welcome to my first console application");
         }
     }
 }
-
