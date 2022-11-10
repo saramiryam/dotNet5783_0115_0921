2b9857571160109abc1af3e517c1184aadca2108
rovayala <rinarov11@gmail.com>
2022-11-10 16:50:30 +0200

    main

--- DalTest/Program.cs
+++ DalTest/Program.cs
@@ -1,6 +1,21 @@
 ﻿// See https://aka.ms/new-console-template for more information
-namespace DalTest;
-public static void Main(string[] args)
-{
+using DO;
+using System.Linq.Expressions;
+using Dal;
 
+using System.Net.Http.Headers;
+namespace DalTest
+{
+    public class Program
+    {
+        static private Product Product = new Product();
+        static private Order Order = new Order();
+        static private OrderItem OrderItem = new OrderItem();
+        static void Main(string[] args)
+        {
+
+
+
+        }
+    }
 }
