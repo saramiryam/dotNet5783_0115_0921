2419a4f73400d6b60aecf5a3ae7a8851d52ba93c
saramiryam <2141saram@gmail.com>
2022-11-12 22:48:23 +0200

    product documentation

--- DalFacade/DO/Product.cs
+++ DalFacade/DO/Product.cs
@@ -7,20 +7,27 @@ namespace DO;
 
 public struct Product
 {
+    #region product properties
+
     public int ID { get; set; }
-    /// <summary>
-    /// the product name - a string
-    /// </summary>
     public string Name { get; set; }
     public ECategory Category { get; set; }
     public double Price { get; set; }
     public int InStock { get; set; }
 
+    #endregion
 
+    #region methods
+    /// <summary>
+    /// override the string function
+    /// </summary>
+    /// <returns>string with the properties of the  Product struct</returns>
     public override string ToString() => $@"
     Product ID={ID}: {Name}, 
     category - {Category}
     Price: {Price}
     Amount in stock: {InStock}
 ";
+    #endregion
+
 }
