aded3da93618bb862c6a537006b85a0f231039fb
שרה מרים צבאח <214120115@malot.org.il>
2022-11-23 18:47:20 +0200

    interface IBl

---
+++ BL/BlApi/IBl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlApi
+{
+    public interface IBl
+    {
+        ICart Cart { get; }
+        IOdert Order { get; }
+        IOrderForList OrderForList { get; }
+        IOrderItem OrderItem { get; }
+        IOrderTracking OrderTracking { get; }
+        IProduct Product{ get; }
+        IProductForList ProductForList { get; }
+        IProductItem ProductItem { get; }
+        
+    }
+}
