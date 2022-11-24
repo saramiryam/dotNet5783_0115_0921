b6151717184282c9c82a4a9345e159edca46c2c3
שרה מרים צבאח <214120115@malot.org.il>
2022-11-24 14:59:02 +0200

    continue delede

--- BL/BlApi/IBl.cs
+++ BL/BlApi/IBl.cs
@@ -10,12 +10,8 @@ namespace BlApi
     {
         public ICart Cart { get; }
         public IOrder Order { get; }
-        public IOrderForList OrderForList { get; }
-        public IOrderItem OrderItem { get; }
-        public IOrderTracking OrderTracking { get; }
         public IProduct Product{ get; }
-        public IProductForList ProductForList { get; }
-        public IProductItem ProductItem { get; }
+  
         
     }
 }
