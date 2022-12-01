e0ea7d7bb658b90e9f7a3092c9d559f55568c0b2
rovayala <rinarov11@gmail.com>
2022-12-01 15:58:46 +0200

    PL project

--- PL/MProductListWindow.xaml.cs
+++ PL/MProductListWindow.xaml.cs
@@ -10,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace PL
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MProductForList.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MProductListWindow : Window
     {
-        public MainWindow()
+        public MProductListWindow()
         {
             InitializeComponent();
-        }
-
-        private void productList_Click(object sender, RoutedEventArgs e)
-        {
-            new MProductListWindow().Show();
         }
     }
 }
--- PL/MProductWindow.xaml.cs
+++ PL/MProductWindow.xaml.cs
@@ -10,24 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace PL
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MProduct.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MProduct : Window
     {
-        public MainWindow()
+        public MProduct()
         {
             InitializeComponent();
-        }
-
-        private void productList_Click(object sender, RoutedEventArgs e)
-        {
-            new MProductListWindow().Show();
         }
     }
 }
