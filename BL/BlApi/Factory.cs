86697dd55be8df6dfd60f3b12af984bb260f5885
rovrovayala <rinarov11@gmail.com>
2022-12-20 03:16:24 +0200

    bl

--- BL/BlApi/Factory.cs
+++ BL/BlApi/Factory.cs
@@ -1,33 +1,17 @@
-﻿using BO;
+﻿using BlImplementation;
+using BO;
 using DalApi;
 using DO;
 using System.Reflection;
 
-using static BlApi.BlConfig;
+
 namespace BlApi;
 
 public static class Factory
 {
     public static IBl? Get()
     {
-        string blType = s_BlName
-           ?? throw new BlConfigException($"DAL name is not extracted from the configuration");
-        string bl = s_BlPackages[s_BlName]
-           ?? throw new BlConfigException($"Package for {blType} is not found in packages list");
-
-        try
-        {
-            Assembly.Load(bl ?? throw new BlConfigException($"Package {bl} is null"));
-        }
-        catch (Exception)
-        {
-            throw new BlConfigException("Failed to load {dal}.dll package");
-        }
-        Type? type = Type.GetType($"Dal.{bl}, {bl}")
-        ?? throw new BlConfigException($"Class Dal.{bl} was not found in {bl}.dll");
-
-        return type.GetProperty("Instance", BindingFlags.Public | BindingFlags.Static)?
-                   .GetValue(null) as IBl
-            ?? throw new BlConfigException($"Class {bl} is not singleton or Instance property not found");
+        Bl l = new Bl();
+        return l;
     }
 }
